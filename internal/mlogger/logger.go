a27d81bebde5d97ed319542471596161688b0f5d
Emmanuel Ebenezer <ekefan.dev@gmail.com>
2026-05-12 14:45:21 +0100

    refactor: place developing custom logger wrapper in internal package

---
+++ internal/mlogger/logger.go
@@ -0,0 +1,29 @@
+package mlogger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/ekefan/marbl/config"
+)
+
+func BuildLogger(cfg config.LoggingConfig) *slog.Logger {
+	var level slog.Level
+	switch cfg.Level {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{Level: level}
+
+	if cfg.Format == "json" {
+		return slog.New(slog.NewJSONHandler(os.Stdout, opts))
+	}
+	return slog.New(slog.NewTextHandler(os.Stdout, opts))
+}
